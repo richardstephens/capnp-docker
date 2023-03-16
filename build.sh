d99f193d9fdc8cd84e0ab8e50b0aed1a4df55c26
Richard Stephens <richard@richst.dev>
2023-03-16 21:57:45 +0100

    remove armv7 - it doesn't seem to build

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker buildx build --push \
   --tag rjst/capnpc:latest \
-  --platform linux/amd64,linux/arm/v7,linux/arm64 .
+  --platform linux/amd64,linux/arm64 .
 
 
 docker buildx build --push \
   --tag rjst/capnpc:capnp-v0.10.3-capnp-java-v0.1.15 \
-  --platform linux/amd64,linux/arm/v7,linux/arm64 .
+  --platform linux/amd64,linux/arm64 .
